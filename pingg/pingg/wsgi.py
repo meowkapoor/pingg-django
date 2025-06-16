d8ab1fac8ef38a38623e6251a37d645d091cd037
meowkapoor <2205885@kiit.ac.in>
2025-06-16 15:31:44 +0530

    deplying 3

--- pingg/pingg/wsgi.py
+++ pingg/pingg/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pingg.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pingg.pingg.settings')
 
 application = get_wsgi_application()
